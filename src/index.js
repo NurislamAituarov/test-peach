782a0fa5b370f8bc7153dfd39d4a41eec3cb1e37
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-12-08 16:36:38 +0600

    add made information business pop up

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-import 'normalize.css'
+import 'normalize.css';
 import './styles/main.scss';
+
+const business_areas_items = document.querySelectorAll('.business-areas__item');
+
+business_areas_items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const currentTarget = e.currentTarget;
+    if (currentTarget.classList.contains('active')) {
+      currentTarget.classList.remove('active');
+    } else {
+      closeBusinessAreaInfo();
+      currentTarget.classList.add('active');
+    }
+  });
+});
+
+function closeBusinessAreaInfo() {
+  business_areas_items.forEach((item) => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+    }
+  });
+}
